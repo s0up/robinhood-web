7237f6c8b562cbc78c0ec3bea9cd5278e3d2a848
Justin Edwards <ws@Justins-MacBook-Pro-2.local>
2017-07-14 19:13:14 -0400

    framing up bootstrap

--- webpack/src/App.vue
+++ webpack/src/App.vue
@@ -1,23 +1,49 @@
 <template>
-  <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <router-view></router-view>
+<div id="app">
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Robinhood</a>
+      </div>
+      <div id="navbar" class="navbar-collapse collapse">
+        <form class="navbar-form navbar-right">
+          <div class="form-group">
+            <input type="text" placeholder="Email" class="form-control">
+          </div>
+          <div class="form-group">
+            <input type="password" placeholder="Password" class="form-control">
+          </div>
+          <button type="submit" class="btn btn-success">Sign in</button>
+        </form>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
+    <div class="container">
+      <div class="row">
+        <!-- <img src="./assets/logo.png"> -->
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
-</template>
-
-<script>
-export default {
+  </template>
+  <script>
+  export default {
   name: 'app'
-}
-</script>
-
-<style>
-#app {
+  }
+  </script>
+  <style>
+  /*#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-</style>
+  }*/
+  </style>
