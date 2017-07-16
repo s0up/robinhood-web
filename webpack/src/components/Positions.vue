02bbeae16922ad5090e3773ecd82d0f0e9b8cd49
Justin Edwards <ws@Justins-MacBook-Pro-2.local>
2017-07-16 09:51:26 -0400

    started work on grid component

--- webpack/src/components/Positions.vue
+++ webpack/src/components/Positions.vue
@@ -1,22 +1,36 @@
+<!-- TODO: figure out limits of template tag and what's allowed outside of it -->
+
 <template>
   <div class="hello">
     <h1>Positions page</h1>
     <p>{{ msg }}</p>
     <div class="row">
+      <script type="text/x-template" id="grid-template">
       <table class="table table-striped table-condensed table-hover">
         <thead>
         <tr>
-          <th>Symbol</th>
-          <th>Shares</th>
-          <th>Current</th>
+          <th v-for="key in columns"
+          @click="sortBy(key)"
+          :class="{ active: sortKey == key }">
+          {{ key | capitalize }}
+          <span class="arrow" :class="sortOrders[key] > 0 ? 'asc' : 'dsc'">
+          </span>
+          </th>
         </tr>
         </thead>
         <tbody>
+          <tr v-for="entry in filteredData">
+            <td v-for="key in columns">
+              {{entry[key]}}
+            </td>
+          </tr>
         </tbody>
       </table>
+      </script>
     </div>
   </div>
 </template>
+
 
 <script>
 export default {
