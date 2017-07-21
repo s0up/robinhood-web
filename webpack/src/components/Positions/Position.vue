3595e8b82a12b0e179bc4b58a06fd338a8027ce1
s0up <mike@haxt.net>
2017-07-20 20:42:29 -0700

    pull fundamentals/instrument

--- webpack/src/components/Positions/Position.vue
+++ webpack/src/components/Positions/Position.vue
@@ -10,12 +10,38 @@
 <script>
 import robinhood from '@/api/robinhood';
 
+/*Example data
+Instrument Response: 
+"result": {
+ "min_tick_size": null,
+ "type": "stock",
+ "splits": "https://api.robinhood.com/instruments/e4a542ca-0e11-454a-9b23-08823027fbb5/splits/",
+ "margin_initial_ratio": "0.5000",
+ "url": "https://api.robinhood.com/instruments/e4a542ca-0e11-454a-9b23-08823027fbb5/",
+ "quote": "https://api.robinhood.com/quotes/HRL/",
+ "symbol": "HRL",
+ "bloomberg_unique": "EQ0010075300001000",
+ "list_date": "1994-01-03",
+ "name": "Hormel Foods",
+ "fundamentals": "https://api.robinhood.com/fundamentals/HRL/",
+ "state": "active",
+ "country": "US",
+ "day_trade_ratio": "0.2500",
+ "tradeable": true,
+ "maintenance_ratio": "0.2500",
+ "id": "e4a542ca-0e11-454a-9b23-08823027fbb5",
+ "market": "https://api.robinhood.com/markets/XNYS/",
+ "simple_name": "Hormel"
+}
+*/
+
 export default {
    name: 'position',
    props: ['row'],
    data(){
       return {
-         instrument: null
+         instrument: null,
+         fundamentals: null
       }
    },
    created(){
@@ -28,6 +54,16 @@ export default {
    computed:{
       position: function(){
          return this.row;
+      }
+   },
+   watch: {
+      instrument: function(instrument){
+         var self = this;
+
+         //Get the fundamental data once the instrument data is present
+         robinhood.getResource(instrument.fundamentals, function(data){
+            self.fundamentals = data.result;
+         });
       }
    }
 }
