52061e2d8b9d1fc80d1e8abb1c8bdf68377cded2
s0up <mike@haxt.net>
2017-08-06 13:59:28 -0700

    generate local.js if no exist

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -8,10 +8,35 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
+const fs = require('fs');
 
 module.exports.bootstrap = function(cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+
+  let localJS = `
+    module.exports = {
+      models: {
+        connection: 'robinhoodMySQL'
+      },
+
+      connections: {
+        robinhoodMySQL: {
+          adapter: 'sails-mysql',
+          host: 'localhost',
+          user: 'root', //optional
+          password: 'm1k3h4x', //optional
+          database: 'robinhood' //optional
+        }
+      }
+    };
+  `;
+
+  if(fs.existsSync(sails.config.appPath + '/config/local.js') === false){
+    sails.log.debug("You should edit your /config/local.js file.  A mockup one has been generated for you!");
+    fs.writeFileSync(sails.config.appPath + '/config/local.js', localJS);
+  }
+
   cb();
 };
