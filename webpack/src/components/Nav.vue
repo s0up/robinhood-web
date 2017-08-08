b53d1027df86dd8849c6d804382bb541db286d9f
Justin Edwards <ws@Justins-MacBook-Pro-2.local>
2017-08-08 12:33:08 -0400

    smaller search box

--- webpack/src/components/Nav.vue
+++ webpack/src/components/Nav.vue
@@ -14,9 +14,9 @@
       <div id="navbar" class="navbar-collapse collapse">
         <div class="navbar-form navbar-left" role="search"  @keyup.enter="search">
             <div class="form-group">
-                <input v-model="ticker_search" type="text" class="form-control" placeholder="Ticker Search">
+                <input v-model="ticker_search" type="text" class="form-control input-sm" placeholder="Ticker Search">
             </div>
-            <button @click="search" type="submit" class="btn btn-default">
+            <button @click="search" type="submit" class="btn btn-default btn-sm">
                 <span class="glyphicon glyphicon-search"></span>
             </button>
         </div>
