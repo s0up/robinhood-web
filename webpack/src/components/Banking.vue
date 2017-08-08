3e2f1b23f679d53bba3edff0cae87a3f2bc1d7fd
Justin Edwards <justinledwards@gmail.com>
2017-08-08 13:04:15 -0400

    money transfer needs vueified

--- webpack/src/components/Banking.vue
+++ webpack/src/components/Banking.vue
@@ -1,5 +1,9 @@
 <template>
   <div class='banking text-center'>
+    <ul class="nav nav-tabs nav-justified">
+      <li role="presentation" class="active"><a href="#">Money to Robinhood</a></li>
+      <li role="presentation"><a href="#">Money to Bank</a></li>
+    </ul>
     <h1 class="display-3 text-center">Coming soon</h1>
     <p class="lead text-center text-danger">Someone should build this page for me :)</p>
     <p>The API calls have been added for automatic and manual ACH transfers</p>
