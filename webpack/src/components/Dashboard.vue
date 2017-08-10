b8412b1fbf93e0858ac6884a227966da8bf613f7
s0up <mike@haxt.net>
2017-08-09 18:33:41 -0700

    line chart kinda works

--- webpack/src/components/Dashboard.vue
+++ webpack/src/components/Dashboard.vue
@@ -1,38 +1,49 @@
 <template>
 <div class='dashboard text-center'>
   <h3 class="text-center">Welcome to Robinhood-Web</h3>
-  <p class="text-danger">Dashboard stats are coming soon...</p>
+  <div v-if="dayLineGraphData && dayChartOptions" class="small">
+    <line-chart :chart-data="dayLineGraphData" :options="dayChartOptions"></line-chart>
+  </div>
 </div>
 </template>
 <script>
 import robinhood from '@/api/robinhood';
 import state from '@/state';
+import LineChart from '@/components/Test/StockChart/LineChart';
 
 export default {
-  created(){
+  created() {
     robinhood.getHistoricals({
       account_number: this.account.account_number,
       interval: '5minute',
       span: 'day'
     });
   },
+  data() {
+    return {
+      dayChartOptions: null
+    }
+  },
   computed: {
-    account(){
+    account() {
       return state.getters.currentAccount;
     },
-    robinhoodUser(){
+    robinhoodUser() {
       return state.getters.robinhoodUser;
     },
-    portfolio(){
+    portfolio() {
       return state.getters.resource(this.account.portfolio);
     },
 
-    dayHistoricals(){
-      return state.getters.historical({interval: '5minute', span: 'day'});
+    dayHistoricals() {
+      return state.getters.historical({
+        interval: '5minute',
+        span: 'day'
+      });
     },
 
-    dayLineGraphData(){
-      if(!this.dayHistoricals){ //Dont try to build the line graph data before day historicals exists
+    dayLineGraphData() {
+      if (!this.dayHistoricals) { //Dont try to build the line graph data before day historicals exists
         return null;
       }
 
@@ -40,21 +51,60 @@ export default {
     }
   },
   methods: {
-    getLineGraphData(data){
+    getLineGraphData(data) {
       let lineGraphData = [];
 
-      data.forEach(function(item){
-        lineGraphData.push({
-          time: item.begins_at,
-          net: item.net_return
-        });
+      let equityData = [];
+      let equityLabelData = [];
+      let min = 0;
+      let max = 0;
+
+      data.forEach(function(item) {
+        if (min == 0) {
+          min = item.adjusted_open_equity;
+        }else if(item.adjusted_open_equity < min){
+          min = item.adjusted_open_equity;
+        }
+
+        if (max == 0) {
+          max = item.adjusted_open_equity;
+        }else if(item.adjusted_open_equity > max){
+          max = item.adjusted_open_equity;
+        }
+
+        equityData.push(parseFloat(item.adjusted_open_equity));
+        equityLabelData.push(item.begins_at);
       });
 
-      return lineGraphData;
+      this.dayChartOptions = {
+        scales: {
+          yAxes: [{
+            display: true,
+            ticks: {
+              min: parseFloat(min),
+              max: parseFloat(max)
+            }
+          }]
+        }
+      }
+
+      lineGraphData.push({
+        label: 'Equity',
+        backgroundColor: '#f00000',
+        data: equityData
+      });
+
+      return {
+        labels: equityLabelData,
+        datasets: lineGraphData
+      }
     }
   },
   watch: {
 
+  },
+  components: {
+    'line-chart': LineChart
   }
 }
 </script>
